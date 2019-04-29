1b977cc59b8d956a808a2c8deb640c6672710a63
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2019-04-29 11:40:33 +0200

    Update program.py

--- program.py
+++ program.py
@@ -46,7 +46,6 @@ class BombaNr3:
             return 1
         else:
             return 0
-print (b)
 print(a[0])
 print(a[1])
 print(a[2])
@@ -59,6 +58,7 @@ for i in range (1,3):
 			bomb=BombaNr0()
 			if (b[0]==0):
 				for h in range (1,9):
+					if test[
 					d=d+1
 					if bomb.ProbaRoz(h)==1:
 						b[0]=h
@@ -85,11 +85,45 @@ for i in range (1,3):
 				a[i][j]=9
 				d=d+1
 				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+		if a[i][j]==2:
+			bomb=BombaNr2()
+			if (b[2]==0):
+				for h in range (1,9):
+					d=d+1
+					if bomb.ProbaRoz(h)==1:
+						b[2]=h
+						a[i][j]=9
+						print ("2Bomba na polu nr: ",i,j," została rozbrojona")
+						break
+			else:
+				bomb.ProbaRoz(b[2])
+				a[i][j]=9
+				d=d+1
+				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+		if a[i][j]==3:
+			bomb=BombaNr3()
+			if (b[3]==0):
+				for h in range (1,9):
+					d=d+1
+					if bomb.ProbaRoz(h)==1:
+						b[3]=h
+						a[i][j]=9
+						print ("Bomba na polu nr: ",i,j," została rozbrojona")
+						break
+			else:
+				bomb.ProbaRoz(b[3])
+				a[i][j]=9
+				d=d+1
+				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+				
 print ("Bomby zostały rozbrojone za podejściem nr: ",d)
 print(a[0])
 print(a[1])
 print(a[2])
 print(a[3])
 print (b)
-print("Do rozbrojenia bomby numer 0, użyjemy narzędzia nr.:",b[0])
-print("Do rozbrojenia bomby numer 1, użyjemy narzędzia nr.:",b[1])
+for i in range (0,4):
+	if b[i]!=0:
+		print("Do rozbrojenia bomby numer",i,", użyjemy narzędzia nr.:",b[i])
+	else:
+		print("Nie było bomby nr.:",i,", lub w danych testowych było za mało informacji")
