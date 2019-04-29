85d01b9318a6cc5b3c8ba4979117098718706730
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2019-04-29 11:38:59 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,16 +1,48 @@
 print("7-gracz\n8-ściana\n9-puste pole\n0-1-bomby")
-b=[0,0]
-a = [[8,8,8,8], [8,7,1,8], [8,1,0,8],[8,8,8,8]]
-zapas=a
+b=[0,0,0,0]
+a = [[8,8,8,8], [8,7,1,8], [8,2,0,8],[8,8,8,8]]
+test=[
+	[0,1,"no"],
+	[1,2,"no"],
+	[0,8,"yes"],
+	[0,4,"no"],
+	[0,6,"no"],
+	[1,2,"no"],
+	[1,5,"no"],
+	[1,2,"no"],
+	[2,4,"no"],
+	[2,8,"no"],
+	[3,7,"no"],
+	[2,6,"no"],
+	[3,4,"no"],
+	[3,3,"no"],
+	[3,5,"yes"]
+	]
+for i in range(0,8):
+	if test[i][2]=="yes":
+		b[test[i][0]]=test[i][1]
+print (b)
 class BombaNr0:
     def ProbaRoz(self,x):
-        if(int(x)==8):
+        if(int(x)==2):
             return 1
         else:
             return 0
 class BombaNr1:
     def ProbaRoz(self,x):
         if(int(x)==3):
+            return 1
+        else:
+            return 0
+class BombaNr2:
+    def ProbaRoz(self,x):
+        if(int(x)==8):
+            return 1
+        else:
+            return 0
+class BombaNr3:
+    def ProbaRoz(self,x):
+        if(int(x)==5):
             return 1
         else:
             return 0
