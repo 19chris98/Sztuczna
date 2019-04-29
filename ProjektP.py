9ae4c524d61f8c999325d7a95f743a7073e01537
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2019-04-29 04:05:13 +0200

    Update ProjektP.py

--- ProjektP.py
+++ ProjektP.py
@@ -1,6 +1,6 @@
 print("7-gracz\n8-ściana\n9-puste pole\n0-1-bomby")
-b=[0,0]
-a = [[8,8,8,8], [8,7,1,8], [8,1,0,8],[8,8,8,8]]
+b=[0,0,0,0]
+a = [[8,8,8,8], [8,7,1,8], [8,2,0,8],[8,8,8,8]]
 test=[
 	[0,1,"no"],
 	[1,2,"no"],
@@ -9,7 +9,14 @@ test=[
 	[0,6,"no"],
 	[1,2,"no"],
 	[1,5,"no"],
-	[1,2,"no"]
+	[1,2,"no"],
+	[2,4,"no"],
+	[2,8,"no"],
+	[3,7,"no"],
+	[2,6,"no"],
+	[3,4,"no"],
+	[3,3,"no"],
+	[3,5,"yes"]
 	]
 for i in range(0,8):
 	if test[i][2]=="yes":
@@ -27,7 +34,18 @@ class BombaNr1:
             return 1
         else:
             return 0
-
+class BombaNr2:
+    def ProbaRoz(self,x):
+        if(int(x)==8):
+            return 1
+        else:
+            return 0
+class BombaNr3:
+    def ProbaRoz(self,x):
+        if(int(x)==5):
+            return 1
+        else:
+            return 0
 print(a[0])
 print(a[1])
 print(a[2])
@@ -66,11 +84,45 @@ for i in range (1,3):
 				a[i][j]=9
 				d=d+1
 				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+		if a[i][j]==2:
+			bomb=BombaNr2()
+			if (b[2]==0):
+				for h in range (1,9):
+					d=d+1
+					if bomb.ProbaRoz(h)==1:
+						b[2]=h
+						a[i][j]=9
+						print ("2Bomba na polu nr: ",i,j," została rozbrojona")
+						break
+			else:
+				bomb.ProbaRoz(b[2])
+				a[i][j]=9
+				d=d+1
+				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+		if a[i][j]==3:
+			bomb=BombaNr3()
+			if (b[3]==0):
+				for h in range (1,9):
+					d=d+1
+					if bomb.ProbaRoz(h)==1:
+						b[3]=h
+						a[i][j]=9
+						print ("Bomba na polu nr: ",i,j," została rozbrojona")
+						break
+			else:
+				bomb.ProbaRoz(b[3])
+				a[i][j]=9
+				d=d+1
+				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+				
 print ("Bomby zostały rozbrojone za podejściem nr: ",d)
 print(a[0])
 print(a[1])
 print(a[2])
 print(a[3])
 print (b)
-print("Do rozbrojenia bomby numer 0, użyjemy narzędzia nr.:",b[0])
-print("Do rozbrojenia bomby numer 1, użyjemy narzędzia nr.:",b[1])
+for i in range (0,4):
+	if b[i]!=0:
+		print("Do rozbrojenia bomby numer",i,", użyjemy narzędzia nr.:",b[i])
+	else:
+		print("Nie było bomby nr.:",i,", lub w danych testowych było za mało informacji")
