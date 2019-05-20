41cc974e3139279afefe03b7983a51bbb930bdb9
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2019-05-20 11:41:52 +0200

    Update ProjektP.py
    dodadnie drzewa do projektu

--- ProjektP.py
+++ ProjektP.py
@@ -1,27 +1,14 @@
 print("7-gracz\n8-ściana\n9-puste pole\n0-1-bomby")
-b=[0,0,0,0]
-a = [[8,8,8,8], [8,7,1,8], [8,2,0,8],[8,8,8,8]]
-test=[
-	[0,1,"no"],
-	[1,2,"no"],
-	[0,8,"yes"],
-	[0,4,"no"],
-	[0,6,"no"],
-	[1,2,"no"],
-	[1,5,"no"],
-	[1,2,"no"],
-	[2,4,"no"],
-	[2,8,"no"],
-	[3,7,"no"],
-	[2,6,"no"],
-	[3,4,"no"],
-	[3,3,"no"],
-	[3,5,"yes"]
-	]
-for i in range(0,8):
-	if test[i][2]=="yes":
-		b[test[i][0]]=test[i][1]
-print (b)
+a = [[8,8,8,8,8,8,8,8],
+     [8,7,1,8,9,9,9,8],
+     [8,2,0,9,9,3,1,8],
+     [8,9,9,9,9,9,9,8],
+     [8,9,9,9,9,9,9,8],
+     [8,9,9,9,9,9,9,8],
+     [8,9,9,9,9,9,1,8],
+     [8,8,8,8,8,8,8,8]
+     ]
+#klasy bomb
 class BombaNr0:
     def ProbaRoz(self,x):
         if(int(x)==2):
@@ -46,82 +33,91 @@ class BombaNr3:
             return 1
         else:
             return 0
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
-d=0
-for i in range (1,3):
-	for j in range (1,3):
+
+#wypisanie planszy
+for i in range(0,8):
+    print(a[i])
+
+#rozbrajanie
+for i in range (1,7):
+	for j in range (1,7):
 		if a[i][j]==0:
 			bomb=BombaNr0()
-			if (b[0]==0):
-				for h in range (1,9):
-					d=d+1
-					if bomb.ProbaRoz(h)==1:
-						b[0]=h
-						a[i][j]=9
-						print ("Bomba na polu nr: ",i,j," została rozbrojona")
-						break
-			else:
-				bomb.ProbaRoz(b[0])
-				a[i][j]=9
-				d=d+1
-				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+			for k in range (1,8):
+				if k>=6:
+					break;
+				else:
+					if k>=2:
+						if k>=4:
+							if a[i][j]>=3:
+								bomb.ProbaRoz(5)
+								a[i][j]=9
+						else:
+							if a[i][j]>=1:
+								bomb.ProbaRoz(3)
+								a[i][j]=9
+							else:
+								if a[i][j]==1:
+									bomb.ProbaRoz(2)
+									a[i][j]=9
 		if a[i][j]==1:
 			bomb=BombaNr1()
-			if (b[1]==0):
-				for h in range (1,9):
-					d=d+1
-					if bomb.ProbaRoz(h)==1:
-						b[1]=h
-						a[i][j]=9
-						print ("Bomba na polu nr: ",i,j," została rozbrojona")
-						break
-			else:
-				bomb.ProbaRoz(b[1])
-				a[i][j]=9
-				d=d+1
-				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+			for k in range (1,8):
+				if k>=6:
+					break;
+				else:
+					if k>=2:
+						if k>=4:
+							if a[i][j]>=3:
+								bomb.ProbaRoz(5)
+								a[i][j]=9
+						else:
+							if a[i][j]>=1:
+								bomb.ProbaRoz(3)
+								a[i][j]=9
+							else:
+								if a[i][j]==1:
+									bomb.ProbaRoz(2)
+									a[i][j]=9
 		if a[i][j]==2:
 			bomb=BombaNr2()
-			if (b[2]==0):
-				for h in range (1,9):
-					d=d+1
-					if bomb.ProbaRoz(h)==1:
-						b[2]=h
-						a[i][j]=9
-						print ("2Bomba na polu nr: ",i,j," została rozbrojona")
-						break
-			else:
-				bomb.ProbaRoz(b[2])
-				a[i][j]=9
-				d=d+1
-				print ("Bomba na polu nr: ",i,j," została rozbrojona")
+			for k in range (1,8):
+				if k>=6:
+					break;
+				else:
+					if k>=2:
+						if k>=4:
+							if a[i][j]>=3:
+								bomb.ProbaRoz(5)
+								a[i][j]=9
+						else:
+							if a[i][j]>=1:
+								bomb.ProbaRoz(3)
+								a[i][j]=9
+							else:
+								if a[i][j]==1:
+									bomb.ProbaRoz(2)
+									a[i][j]=9
 		if a[i][j]==3:
 			bomb=BombaNr3()
-			if (b[3]==0):
-				for h in range (1,9):
-					d=d+1
-					if bomb.ProbaRoz(h)==1:
-						b[3]=h
-						a[i][j]=9
-						print ("Bomba na polu nr: ",i,j," została rozbrojona")
-						break
-			else:
-				bomb.ProbaRoz(b[3])
-				a[i][j]=9
-				d=d+1
-				print ("Bomba na polu nr: ",i,j," została rozbrojona")
-				
-print ("Bomby zostały rozbrojone za podejściem nr: ",d)
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
-print (b)
-for i in range (0,4):
-	if b[i]!=0:
-		print("Do rozbrojenia bomby numer",i,", użyjemy narzędzia nr.:",b[i])
-	else:
-		print("Nie było bomby nr.:",i,", lub w danych testowych było za mało informacji")
+			for k in range (1,8):
+				if k>=6:
+					break;
+				else:
+					if k>=2:
+						if k>=4:
+							if a[i][j]>=3:
+								bomb.ProbaRoz(5)
+								a[i][j]=9
+						else:
+							if a[i][j]>=1:
+								bomb.ProbaRoz(3)
+								a[i][j]=9
+							else:
+								if a[i][j]==1:
+									bomb.ProbaRoz(2)
+									a[i][j]=9
+
+#wypisanie planszy
+for i in range(0,8):
+    print(a[i])
